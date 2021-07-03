4ffe61ec1f33eac502e958c042a8ef7ea6cac1d5
Pier-Olivier St-Pierre Chouinard <pier.olivier.stpch@gmail.com>
2021-07-03 15:37:18 -0400

    Ajout des attributs pour Livre.

--- Bibliotheque_LIPAJOLI/Models/Livre.cs
+++ Bibliotheque_LIPAJOLI/Models/Livre.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bibliotheque_LIPAJOLI.Models
 {
     public class Livre
     {
+        [Required, Key]
+        public string CodeLivre { get; set; }
+        public string Isbn10 { get; set; }
+        public string Isbn13 { get; set; }
+        public string Titre { get; set; }
+        public string Categorie { get; set; }
+        public int Quantite { get; set; }
+        public double Prix { get; set; }
+        public List<string> Auteurs { get; set; }
     }
 }
+
