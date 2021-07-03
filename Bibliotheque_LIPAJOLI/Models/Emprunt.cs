7fbc1999a47b8a9ff9f8b77a85d77cbd73e54914
Pier-Olivier St-Pierre Chouinard <pier.olivier.stpch@gmail.com>
2021-07-03 15:58:43 -0400

    Ajout du modèle Emprunt et de la logique pour obtenir la date limite de retour d'un livre.

---
+++ Bibliotheque_LIPAJOLI/Models/Emprunt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Configuration;
+
+namespace Bibliotheque_LIPAJOLI.Models
+{
+    public class Emprunt
+    {
+        [Required]
+        public string CodeLivre { get; set; }
+        [Required]
+        public string NumAbonne { get; set; }
+        [Required]
+        public DateTime DateEmprunt { get; set; }
+        public DateTime DateRetour { get; set; }
+
+        public DateTime ObtenirDateLimite(IConfiguration config)
+        {
+            var nbJoursLocation = config.GetSection("Bibliotheque")
+                .GetValue<int>("JoursLocation");
+
+            return DateEmprunt.AddDays(nbJoursLocation);
+        }
+    }
+}
