95d9b9b978ebd40d16f579f051218a2df3b3beb6
Pier-Olivier St-Pierre Chouinard <pier.olivier.stpch@gmail.com>
2021-07-03 16:28:52 -0400

    Ajout du contexte de dabe de données au démarrage, ajout de la chaîne de connexion dans les configurations.

--- Bibliotheque_LIPAJOLI/Startup.cs
+++ Bibliotheque_LIPAJOLI/Startup.cs
@@ -1,5 +1,7 @@
+using Bibliotheque_LIPAJOLI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,8 @@ namespace Bibliotheque_LIPAJOLI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<BibliothequeContext>(options => 
+                    options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
