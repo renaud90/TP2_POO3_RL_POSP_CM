fafd84c2293f3afed42780b376674e8c242718f0
Renaud Lussier <renaud_lussier90@hotmail.com>
2021-07-03 08:42:00 -0400

    Ajoutez des fichiers projet.

---
+++ Bibliotheque_LIPAJOLI/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Bibliotheque_LIPAJOLI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bibliotheque_LIPAJOLI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Bibliotheque_LIPAJOLI/Models/Livre.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bibliotheque_LIPAJOLI.Models
+{
+    public class Livre
+    {
+    }
+}
