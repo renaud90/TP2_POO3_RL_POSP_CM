d9c4b732d80f2415e27af8932d42c27ca8ee4b47
Pier-Olivier St-Pierre Chouinard <pier.olivier.stpch@gmail.com>
2021-07-14 16:56:03 -0400

    Modification de l'obtention de la date limite d'un Emprunt.

--- Bibliotheque_LIPAJOLI/Models/Emprunt.cs
+++ Bibliotheque_LIPAJOLI/Models/Emprunt.cs
@@ -18,12 +18,5 @@ namespace Bibliotheque_LIPAJOLI.Models
         public Livre Livre { get; set; }
         public Usager Usager { get; set; }
 
-        public DateTime ObtenirDateLimite(IConfiguration config)
-        {
-            var nbJoursLocation = config.GetSection("Bibliotheque:JoursLocations")
-                .Get<int>();
-
-            return DateEmprunt.AddDays(nbJoursLocation);
-        }
     }
 }
